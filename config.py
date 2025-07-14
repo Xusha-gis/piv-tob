3ac2d69645244f651cddf23a5b61335f440fb297
Xusha-gis <ragimberganovgis01@gmail.com>
2025-07-14 22:21:40 +0500

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8188119649:AAEC5lTlyPTTskGxO_d5APGKXE8iKmZKwbo")
-CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/+tz7C4K2sDElmYjky")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "5515940119"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8188119649:AAEC5lTlyPTTskGxO_d5APGKXE8iKmZKwbo")
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/+tz7C4K2sDElmYjky")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "5515940119"))
